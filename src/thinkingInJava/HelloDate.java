584aa8f6c2ea484fc26ad39798996b6a104e51cc
MaxVishnevsky <vishnevsky10@gmail.com>
2016-01-30 20:11:23 +0200

    Initial commit

---
+++ src/thinkingInJava/HelloDate.java
@@ -0,0 +1,9 @@
+package thinkingInJava;
+
+import java.util.Date;
+
+public class HelloDate {
+    public static void main(String[] args) {
+        System.out.println("Hello, today is: " + new Date());
+    }
+}
